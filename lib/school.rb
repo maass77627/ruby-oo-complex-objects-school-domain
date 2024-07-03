e3c1b5d745853ff923cc246a79929028938661ac
kelly_maass <kmaass77627@gmail.com>
2024-07-02 20:17:25 -0500

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School 
+    attr_reader :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+
+    end
+
+    def add_student(name, grade)
+      @roster[grade] ||= @roster[grade] = []
+      @roster[grade] << name
+
+hash
+
+    end
+
+    def grade(grade)
+       @roster[grade]
+end
+
+    def sort
+
+        @roster.each do |grade, students|
+            students.sort!
+        end
+
+    end
+end
